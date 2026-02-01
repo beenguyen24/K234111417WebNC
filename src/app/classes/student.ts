a2816bec36f0ac092bdfbbc18549369affd3723e
beenguyen24 <thiennna234111e@st.uel.edu.vn>
2026-02-01 23:55:43 +0700

    Add classes folder

---
+++ src/app/classes/student.ts
@@ -0,0 +1,9 @@
+export class Student {
+  constructor(
+    public name: string, //k biet thì để any
+    public email: string,
+    public phone: string,
+    public course: string,
+    public time: string,
+  ) {}
+}
